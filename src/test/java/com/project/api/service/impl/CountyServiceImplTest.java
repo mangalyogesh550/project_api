7c5b2d11892af84a22d904ae95c3e79c5152a588
Yogesh Kumar Mangal <mangalyogesh550@gmail.com>
2022-09-15 22:29:20 +0530

    semi completed with testing

--- src/test/java/com/project/api/service/impl/CountyServiceImplTest.java
+++ src/test/java/com/project/api/service/impl/CountyServiceImplTest.java
@@ -19,56 +19,56 @@ import com.project.api.entity.CountyEntity;
 import com.project.api.repository.CountyRepo;
 
 public class CountyServiceImplTest {
-//	@Mock
-//	private CountyRepo countyRepo;
-//
-//	@InjectMocks
-//	private CountyServiceImpl countyServiceImpl;
-//
-//	@BeforeEach
-//	void setUp() {
-//		MockitoAnnotations.openMocks(this);
-//	}
-//
-//	@Test
-//	void addCountyTest() {
-//		CountyEntity countyEntity = new CountyEntity();
-//		countyEntity.setName("Test Name");
-//		when(countyRepo.save(ArgumentMatchers.any(CountyEntity.class))).thenReturn(countyEntity);
-//		CountyEntity county = countyServiceImpl.addCounty(countyEntity);
-//		assertThat(county.getName()).isSameAs(countyEntity.getName());
-//		verify(countyRepo).save(countyEntity);
-//	}
-//
-//	// method1
-//	@Test
-//	void getAllCountyTest() {
-//		List<CountyEntity> counties = new ArrayList();
-//		counties.add(new CountyEntity()); // this line can be removed not compulsory
-//		when(countyRepo.findAll()).thenReturn(counties);
-//		List<CountyEntity> expected = countyServiceImpl.getAllCounty();
-//		assertEquals(expected, counties);
-//		verify(countyRepo).findAll();
-//	}
-//
-//	@Test
-//	public void getByNameTest() {
-//		CountyEntity county = new CountyEntity();
-//		county.setName("test");
-//		when(countyRepo.findByName(county.getName())).thenReturn(county);
-//		CountyEntity expected = countyServiceImpl.getByName(county.getName());
-//		assertThat(expected).isSameAs(county);
-//		verify(countyRepo).findByName(county.getName());
-//	}
-//
-//	@Test
-//	public void getByStateTest() {
-//		CountyEntity county = new CountyEntity();
-//		county.setState("test");
-//		when(countyRepo.findByState(county.getState())).thenReturn((List<CountyEntity>) county);
-//		List<CountyEntity> expected = countyServiceImpl.getByState(county.getState());
-//		assertThat(expected).isSameAs(county);
-//		verify(countyRepo).findByName(county.getName());
-//	}
+	@Mock
+	private CountyRepo countyRepo;
+
+	@InjectMocks
+	private CountyServiceImpl countyServiceImpl;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void addCountyTest() {
+		CountyEntity countyEntity = new CountyEntity();
+		countyEntity.setName("Test Name");
+		when(countyRepo.save(ArgumentMatchers.any(CountyEntity.class))).thenReturn(countyEntity);
+		CountyEntity county = countyServiceImpl.addCounty(countyEntity);
+		assertThat(county.getName()).isSameAs(countyEntity.getName());
+		verify(countyRepo).save(countyEntity);
+	}
+
+	// method1
+	@Test
+	void getAllCountyTest() {
+		List<CountyEntity> counties = new ArrayList();
+		counties.add(new CountyEntity()); // this line can be removed not compulsory
+		when(countyRepo.findAll()).thenReturn(counties);
+		List<CountyEntity> expected = countyServiceImpl.getAllCounty();
+		assertEquals(expected, counties);
+		verify(countyRepo).findAll();
+	}
+
+	@Test
+	public void getByNameTest() {
+		CountyEntity county = new CountyEntity();
+		county.setName("test");
+		when(countyRepo.findByName(county.getName())).thenReturn(county);
+		CountyEntity expected = countyServiceImpl.getByName(county.getName());
+		assertThat(expected).isSameAs(county);
+		verify(countyRepo).findByName(county.getName());
+	}
+
+	@Test
+	public void getByStateTest() {
+		CountyEntity county = new CountyEntity();
+		county.setState("test");
+		when(countyRepo.findByState(county.getState())).thenReturn((List<CountyEntity>) county);
+		List<CountyEntity> expected = countyServiceImpl.getByState(county.getState());
+		assertThat(expected).isSameAs(county);
+		verify(countyRepo).findByName(county.getName());
+	}
 
 }
